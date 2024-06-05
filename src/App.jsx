dcb8426f6dc576b7c593217ba4d47f54f23f4a8f
Nik6348 <rajputnik911.com>
2024-06-05 09:36:16 +0530

    update client side routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Chat from './components/Chat';
@@ -8,7 +8,7 @@ import ChatList from './components/ChatList';
 
 function App() {
   return (
-      <Router basename="/Chat-App-FE">
+      <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<Signup />} />
