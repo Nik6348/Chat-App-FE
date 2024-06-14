876922c39573dba2a32b5e160b2027b783d2ec3f
Nik6348 <rajputnik911.com>
2024-06-14 13:18:59 +0530

    fix socket config

--- src/hooks/useSocket.jsx
+++ src/hooks/useSocket.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useRef } from 'react';
 import io from 'socket.io-client';
-import { getMessages as getMessagesAPI } from '../api/messageService';
+import {
+  getMessages as getMessagesAPI,
+  updateStatus as updateStatusAPI,
+} from '../api/messageService';
 
 const useSocket = (userId) => {
   const apiUrl = 'https://chat-app-be-nik6348s-projects.vercel.app/';
@@ -19,20 +22,25 @@ const useSocket = (userId) => {
     });
 
     // Listen for new messages
-    socketRef.current.on('receive_message', async (message) => {
+    socketRef.current.on('receive_message', async (msg) => {
       // get decrypted message
-      const message = await getMessagesAPI(message._id);
+      const message = await getMessagesAPI(msg._id);
       setMessages((prevMessages) => [...prevMessages, message]);
     });
 
     // Listen for message status updates
-    socketRef.current.on('update_message_status', ({ messageId, status }) => {
-      setMessages((prevMessages) =>
-        prevMessages.map((message) =>
-          message._id === messageId ? { ...message, status } : message
-        )
-      );
-    });
+    socketRef.current.on(
+      'update_message_status',
+      async ({ messageId, status }) => {
+        // Update the message status in the db
+        await updateStatusAPI(messageId, status);
+        setMessages((prevMessages) =>
+          prevMessages.map((message) =>
+            message._id === messageId ? { ...message, status } : message
+          )
+        );
+      }
+    );
     return () => {
       socketRef.current.disconnect();
     };
